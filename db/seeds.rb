d139af56fda6d6d95981447315602272b205c12f
Benjamin Liet <liet.ben@gmail.com>
2022-05-18 16:35:57 +0200

    Made the fake AirBnB with some personnal style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Droping database'
+
+Flat.destroy_all
+
+puts 'Destroyed !'
+puts 'Add 10 new flats'
+
+10.times do
+  Flat.create(
+    name: Faker::Lorem.paragraph,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 5),
+    price_per_night: %w[10 15 20 25 30 35 40 45 50].sample,
+    number_of_guests: %w[1 2 3 4].sample
+  )
+end
